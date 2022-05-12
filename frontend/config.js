5c699abd6ddcd77878afe5044235b54201e4fc7a
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2022-05-12 15:59:09 +0100

    new config.sidebar format

--- frontend/config.js
+++ frontend/config.js
@@ -43,7 +43,8 @@ exports.auth = {
     title: 'Title from Homepage.js',
     sidebar: [
       {
-        title: 'First Section',
+        title: 'Landing Pages',
+        type: 'landing_pages',
         items: [
           {
             title: 'Splash Page',
@@ -60,24 +61,68 @@ exports.auth = {
         ]
       },
       {
-        title: 'Second Section',
+        title: 'Public Full Page Apps',
+        type: 'public_embeds',
+        items: [
+          {
+            title: 'Public Page 1',
+            icon: 'MailIcon',
+            url: '/public_embed_1',
+            retool_app: 'https://example.retool.com/embedded/public/86784719-70dd-419a-b28f-d4d1e7788729',
+            groups: [],
+          },
+          {
+            title: 'Public Page 2',
+            icon: 'AddToDriveIcon',
+            url: '/public_embed_2',
+            retool_app: 'https://example.retool.com/embedded/public/86784719-70dd-419a-b28f-d4d1e7788729',
+            groups: [],
+          },
+        ]
+      },
+      {
+        title: 'Default Demo Apps',
+        type: 'default_demo_apps',
         items: [
           {
             title: 'Full Page',
             icon: 'MailIcon',
             url: '/full_page_embed',
+            retool_app: 'https://demos.retool.com/apps/3f0173e6-cc7d-11ec-945a-d74c1b48b4d0/Jon/Account%20Dashboard%20copy',
             groups: ['bronze', 'silver', 'gold'],
           },
           {
             title: 'Hybrid',
             icon: 'AddToDriveIcon',
             url: '/hybrid_page',
+            retool_app: 'https://example.retool.com/embedded/public/86784719-70dd-419a-b28f-d4d1e7788729',
             groups: ['silver', 'gold'],
           },
           {
             title: 'Panel',
             icon: 'BarChartIcon',
             url: '/panel_embed',
+            retool_app: 'https://example.retool.com/embedded/public/f808a1bf-8cb7-4de6-8f3e-19a5eea01059',
+            groups: ['gold'],
+          },
+        ]
+      },
+      {
+        title: 'Protected Full Page Apps',
+        type: 'protected_embeds',
+        items: [
+          {
+            title: 'Protected App 1',
+            icon: 'MailIcon',
+            url: '/protected_embed_1',
+            retool_app: 'https://demos.retool.com/apps/3f0173e6-cc7d-11ec-945a-d74c1b48b4d0/Jon/Account%20Dashboard%20copy',
+            groups: ['gold'],
+          },
+          {
+            title: 'Protected App 2',
+            icon: 'AddToDriveIcon',
+            url: '/protected_embed_2',
+            retool_app: 'https://demos.retool.com/apps/3f0173e6-cc7d-11ec-945a-d74c1b48b4d0/Jon/Account%20Dashboard%20copy',
             groups: ['gold'],
           },
         ]
