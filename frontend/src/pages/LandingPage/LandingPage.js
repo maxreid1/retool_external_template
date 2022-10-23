f7d634b3022eec14f05275d8dd6b4277a999e427
maxreid1 <max.reid@retool.com>
2022-10-23 16:23:48 -0400

    added _embed=true

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -9,7 +9,7 @@ const LandingPage = () => {
       <Box sx={{ height: "1200px" }}>
       <Typography variant="h2" align='center' marginTop={'32'} marginBottom={'15'}>Welcome to your Shop Co. Portal!</Typography>
           <Container maxWidth={false}>
-              <Retool url='https://demos.retool.dev/apps/Embed%20Demo%20-%20Merchant%20Portal/1-%20Bronze%20-%20Store%20Overview%202?_releaseVersion=latest?_embed=true' />
+              <Retool url='https://demos.retool.dev/apps/Embed%20Demo%20-%20Merchant%20Portal/1-%20Bronze%20-%20Store%20Overview%202?_embed=true' />
           </Container>
       </Box>
       
