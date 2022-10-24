a2fe431d80b90afe1a520b0676d272b591c65782
maxreid1 <max.reid@retool.com>
2022-10-24 10:34:46 -0400

    changed margins

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -6,9 +6,9 @@ import Retool from 'react-retool'
 
 const LandingPage = () => { 
   return (
-      <Box sx={{ height: "1200px" }}>
-      <Typography variant="h2" align='center' marginTop={'32'} marginBottom={'15'}>Welcome to your Shop Co. Portal!</Typography>
-          <Container maxWidth={false}>
+      <Box sx={{ height: "1200px", marginTop: "-25" }}>
+      {/* <Typography variant="h3" align='center' marginTop={'32'} marginBottom={'0'}>Welcome to your Shop Co. Portal!</Typography> */}
+          <Container maxWidth={false} maxHeight={true} >
               <Retool url='https://demos.retool.dev/apps/Embed%20Demo%20-%20Merchant%20Portal/1-%20Bronze%20-%20Store%20Overview%202?_embed=true' />
           </Container>
       </Box>
