5ae335097ea06f40e0a82ce2fa84fbb0feaf8c1d
antonybello <antony.bello@pomona.edu>
2022-11-18 14:03:04 -0500

    commit away

--- frontend/src/pages/SplashPage/SplashPage.js
+++ frontend/src/pages/SplashPage/SplashPage.js
@@ -1,52 +1,23 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react"
-import { Button, containerClasses, MenuItem } from '@mui/material'
+import { Button, Box, Paper } from '@mui/material'
+import './styles.css'
 
-const LoginMenuItem = () => {
+const SplashPage = () => {
   const { loginWithRedirect } = useAuth0()
 
   return (
-    <MenuItem key='auth0-login'>
-      <Button  onClick={() => loginWithRedirect()} variant="contained" sx={{color:'white'}}>
-        Click Here to Log In
-      </Button>      
-    </MenuItem>
+      <Paper className="bg">
+          <Box className="login-container">
+            <img src="https://i.ibb.co/SBfqNbc/imageedit-1-4156875095.png" width="300px"/>
+            <h3 className="title"> Merchant portal </h3>
+            <p>Log in to manage your restaurants, refund orders, and drive new business.</p>          
+            <Button className="login" key="auth0-login" onClick={() => loginWithRedirect()} variant="contained">
+              Log in
+            </Button>      
+          </Box>
+      </Paper>
   )
-}
-
-import { 
-  Box,
-  Grid,
-  Typography
-} from '@mui/material'
-
-const SplashPage = () => {
-    return (
-        
-        <div style={{ 
-            backgroundImage: `url("https://images.pexels.com/photos/326281/pexels-photo-326281.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")` ,
-            position: 'absolute',
-            backgroundSize: 'cover',
-            width: '100%',
-            height: '120%',
-            marginTop: -65,
-            marginBottom: 65
-          }}>
-            
-                   <Box display={'flex'} justifyContent='right' marginTop='200' marginRight='100'>
-                    < img src="https://i.ibb.co/b2k0Ss0/Screen-Shot-2021-09-10-at-5-29-42-PM-removebg-preview.png" className='center' />
-                    </Box>
-                    <Box display={'flex'} justifyContent='right'  marginRight='300'>
-                    <Typography variant="h6" color="black" gutterBottom >
-                       
-                        <LoginMenuItem></LoginMenuItem>
-                    </Typography>
-                    </Box>
-   
-        {}
-        </div>
-        
-    )
 }
 
 export default SplashPage
